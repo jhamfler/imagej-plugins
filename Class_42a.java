531543502d81930a27dec72e22b310c67dab5de0
Johannes Hamfler <johannes.hamfler@stud.htwk-leipzig.de>
2017-05-28 23:59:20 +0200

    42a done

--- Class_42a.java
+++ Class_42a.java
@@ -25,12 +25,11 @@ public class Class_42a implements ExtendedPlugInFilter, DialogListener {
     private final static int FLAGS = DOES_8G | PARALLELIZE_STACKS;
     private static double toleranceS = 1.;
     private double tolerance = 1.;
-    private int nPasses = 1;
+    private int nPasses = 30;
     private int pass = 0;
     private boolean background255;
     private boolean interrupted;        /*whether watershed segmentation has been interrupted by the user */
-    private MaximumFinder maxFinder =
-            new MaximumFinder();        /*we use only one MaximumFinder (nicer progress bar)*/
+    private MaximumFinder maxFinder = new MaximumFinder();        /*we use only one MaximumFinder (nicer progress bar)*/
 	private ImagePlus thisimp;
     int width, height, xmax, ymax;
 
@@ -59,14 +58,6 @@ public class Class_42a implements ExtendedPlugInFilter, DialogListener {
         height = imp.getHeight();
         xmax = width - 1;
         ymax = height - 1;
-        GenericDialog gd = new GenericDialog(command+"...");
-        gd.addNumericField("Tolerance", toleranceS, 1, 5, "(0.5 is ImageJ standard)");
-            gd.addPreviewCheckbox(pfr);     /* passing pfr makes the filter ready for preview */
-            gd.addDialogListener(this);     /* the DialogItemChanged method will be called on user input */
-            gd.showDialog();                /* display the dialog; preview runs in the  now */
-            if (gd.wasCanceled()) return DONE;
-            toleranceS = tolerance;
-
         return IJ.setupDialog(imp, FLAGS);
     }
 
@@ -79,11 +70,13 @@ public class Class_42a implements ExtendedPlugInFilter, DialogListener {
     }
 
     public void run(ImageProcessor ip) {
+		int erosionen = 0, muenzen = 0;
+
+		// WATERSHED
         if (interrupted) return;
         int backgroundValue = background255 ? (byte)255 : 0;
         FloatProcessor floatEdm = new EDM().makeFloatEDM(ip, backgroundValue, false);
-        ByteProcessor newIp = maxFinder.findMaxima(floatEdm, tolerance,
-                ImageProcessor.NO_THRESHOLD, MaximumFinder.SEGMENTED, false, true);
+        ByteProcessor newIp = maxFinder.findMaxima(floatEdm, tolerance, ImageProcessor.NO_THRESHOLD, MaximumFinder.SEGMENTED, false, true);
         if (newIp == null) {  //segmentation cancelled by user?
             interrupted = true;
             return;
@@ -91,24 +84,28 @@ public class Class_42a implements ExtendedPlugInFilter, DialogListener {
         drawSegmentationLines(ip, backgroundValue, newIp);
         ip.setBinaryThreshold();
 
-		// PARTIKEL HIER
+		// loop until no coin is left
+		do {
+
+		// COUNT PARTICLES
 		ImagePlus plus = IJ.getImage();
-		ResultsTable rt = ResultsTable.getResultsTable();
-		if(rt==null) rt = new ResultsTable();
+//		ResultsTable rt = ResultsTable.getResultsTable();
+//		if(rt==null)
+		ResultsTable rt = new ResultsTable();
 		RoiManager manager = RoiManager.getInstance();
 		//if(manager==null) manager = new RoiManager();
 
-		ParticleAnalyzer analyser=new ParticleAnalyzer(0, Measurements.AREA, rt, 1, Double.MAX_VALUE, 0, 1);
-//		ParticleAnalyzer analyser=new ParticleAnalyzer(ParticleAnalyzer.SHOW_RESULTS+ParticleAnalyzer.ADD_TO_MANAGER, Measurements.AREA, rt, 10, Double.MAX_VALUE, 0, 1);
+		ParticleAnalyzer analyser = new ParticleAnalyzer(0, Measurements.AREA, rt, 1, Double.MAX_VALUE, 0, 1);
 
 		analyser.analyze(plus);
 		//rt.updateResults();
 		//rt.show("Results");
-		if (rt.size() < 20) rt.show("Results");
+		muenzen = rt.size();
 
-		// ERODIEREN
-		// Pixels with an EDM value < radius will be set to background
-		double radius = 1;
+
+		// ERODE
+		// pixels with an EDM value < radius will be set to background
+/*		double radius = 1;
 		boolean fromEdge = true;
         int width = ip.getWidth();
         int height = ip.getHeight();
@@ -119,9 +116,12 @@ public class Class_42a implements ExtendedPlugInFilter, DialogListener {
             for (int x=roiRect.x, p=x+y*width; x<roiRect.x+roiRect.width; x++,p++)
                 if (fPixels[p] <= radius)
                     bPixels[p] = (byte)backgroundValue;
-        return;
-
-
+*/
+		ip.dilate(); // opposite of erode because of inverted LUT
+		erosionen++;
+		System.out.println("Erosionen: " + erosionen + "\tMünzen: " + muenzen);
+		plus.repaintWindow();
+		} while (muenzen > 0);
     }
 
     /** Draw the segmentation lines in the original image.
@@ -132,35 +132,6 @@ public class Class_42a implements ExtendedPlugInFilter, DialogListener {
         byte[] segmPixels = (byte[])segmentedIp.getPixels();
         for (int p=0; p<origPixels.length; p++)
             if (segmPixels[p] == 0) origPixels[p] = (byte)backgroundValue;
-    }
-        /* pixel offsets of the 8 neighbor pixels in direction 'd' = 0...7 for direct addressing */
-/*        int[] dirOffset  = new int[] {-width, -width+1, +1, +width+1, +width, +width-1,   -1, -width-1 };
-        byte foregroundValue = (byte)~backgroundValue;
-        byte smallParticle = 10; /* any value but 0, 255, used to mark small particles */
-/*        FloodFiller floodFiller = new FloodFiller(ip);
-        for (int y=0, p=0; y<height; y++)
-            for (int x=0; x<width; x++,p++)
-                if (origPixels[p] != backgroundValue && origPixels[p] != smallParticle && segmPixels[p] == 0) {
-                    /* This pixel has been set to background. Walk around it; */
-                    /* if we have at least two foreground-background transitions */
-                    /* this is a segmentation line that should be background. */
-                    /* (out-of-image pixels count as background) */
-/*                    int nTransitions = 0;
-                    boolean lastWasForeground = isWithin(x, y, 7) && segmPixels[p+dirOffset[7]]!=0;
-                    for (int d=0; d<8; d++) {
-                        boolean isForeground = isWithin(x, y, d) && segmPixels[p+dirOffset[d]]!=0;
-                        if (isForeground && !lastWasForeground) nTransitions++;
-                        lastWasForeground = isForeground;
-                    }
-                    if (nTransitions<2) {
-                        origPixels[p] = smallParticle;
-                        floodFiller.fill8(x,y); /* mark the whole small particle as such */
-//                    } else
-/*                        origPixels[p] = (byte)backgroundValue;   /* a segmentation line */
-/*                }
-        for (int p=0; p<origPixels.length; p++)
-            if (origPixels[p] == smallParticle) origPixels[p] = foregroundValue;
-        
     }
 
     /* whether a neighbor of pixel x, y in direction 'd' is inside the image */
